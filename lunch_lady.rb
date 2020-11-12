0373f5babcf382d98a91f0026872b74e4dd03924
dani7beth <danielledona28@gmail.com>
2020-11-12 15:16:49 -0700

    added a print menu method

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,16 +1,32 @@
 require_relative 'menu'
 
-lunch_menu = Menu.new
-
-puts lunch_menu.class
-
 #create a main dish menu
+main_dish_menu ={
+    message: "Please select one main dish from the list: ",
+    options: [{
+        dish: ['Meatloaf', 'Chicken', 'Spaghetti', 'Fishsticks', 'Grilled Cheese Sandwich'],
+        price: [6.30, 5.45, 4.50, 4.00, 3.00]
+    }]
+}
+mains = Menu.new(main_dish_menu)
+
+#create a side dish menu
+side_dish_menu ={
+    message: "Please select your side dish from the list: ",
+    options: [{
+        dish: ['Carrots', 'Salad', 'Beans', 'Macaroni', 'Broccoli'],
+        price: [2.00, 3.50, 1.30, 3.00, 2.45]
+    }]
+}
+sides = Menu.new(side_dish_menu)
 
 #call print menu for main dish
+mains.print_menu
 
 #user select main dish
 
 #call print menu for side dish
+sides.print_menu
 
 #user select side dishes
 
--- menu.rb
+++ menu.rb
@@ -2,12 +2,26 @@
 class Menu
 
     #create a menu
-    def initialize
-        
+    def initialize(menu)
+        @menu = menu
     end
 
     #print main dish menu
+    def print_menu
+        #print greeting
+        border = @menu[:message].gsub(/\s|\S/, '*')
+        puts border
+        puts @menu[:message]
+        puts border
 
+        #print dishes very complicated but using nested loops i can 
+        #print the menu and keep it organized the way i want it
+        @menu[:options].each do |item| #this grabs all the items in the options hash
+            item[:dish].each_with_index do |item2, index| #this grabs the dish array within the option hash
+                puts "#{index+1}. #{item2} #{item[:price][index]}"#the item[:price][index] grabs all the prices associated with each menu item
+            end                              
+        end
+    end
 
     #user selection 
     # can only select one main dish 
